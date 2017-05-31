7b9b2a8721bcf5254105026e1ad0684f0dbe98ae
AlonsoPerezDiego <Dialpe22@gmail.com>
2017-05-31 10:00:40 +0200

    Main class que llama a la interface MainMenu para hacerla visible.

---
+++ src/battlemonsters/BattleMonsters.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battlemonsters;
+
+import interfaces.MainMenu;
+
+/**
+ *
+ * @author Diego
+ */
+public class BattleMonsters {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainMenu().setVisible(true);
+    }
+    
+}
