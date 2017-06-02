d8e3c6c78e32601bdad64060c422e9c44518f102
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-06-02 17:12:33 +0200

    Cambios y creacion de metodos de insercion en la base y quitar metodo de mostrar la tabla de crear

--- src/battlemonsters/MetodosCrearBD.java
+++ src/battlemonsters/MetodosCrearBD.java
@@ -36,8 +36,8 @@ public class MetodosCrearBD {
         crearBMonstruos();
         crearBMovimientos();
         crearJugador();
-        insertarMonstruos();
-        mostrarTablas();
+        insertarBMonstruos();
+        insertarBMovimientos();
         desconectar();
     }
     public static void crearBMonstruos(){
@@ -119,15 +119,8 @@ public class MetodosCrearBD {
                     + "plose integer)";
             stmt.executeUpdate(sql);
         } catch (SQLException ex) {
-            try {
-                stmt.executeUpdate("drop table jugador");
-                crearJugador();
-            } catch (SQLException ex1) {
-                System.err.println("Error al borrar la tabla jugador");
-            }
-        }
     }
-
+    }
     public static void desconectar(){
         try {
             stmt.close();
@@ -137,49 +130,29 @@ public class MetodosCrearBD {
         }
     }
 
-    public static void insertarMonstruos(){
+    public static void insertarBMonstruos() {
         try {
-            stmt=conn.createStatement();
-            String sql1 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(1,'Amepach','steel','MonsterJ1/AmepachJ1.png','RivalMonster/Amepach.png',150,30,30,'grass','earth','water','fire')";
-            String sql2 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(2,'Ankylore','earth','MonsterJ1/AnkyloreJ1.png','RivalMonster/Ankylore.png',190,25,45,'fire','water','steel','grass')";
-            String sql3 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(3,'Haisand','earth','MonsterJ1/HaisandJ1.png','RivalMonster/Haisand.png',150,40,25,'fire','water','steel','grass')";
-            String sql4 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(4,'Hakonmyo','steel','MonsterJ1/HakonmyoJ1.png','RivalMonster/Hakonmyo.png',160,45,25,'grass','earth','water','fire')";
-            String sql5 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(5,'Hiprunio','fire','MonsterJ1/HirprunioJ1.png','RivalMonster/Hiprunio.png',170,30,25,'grass','steel','earth','water')";
-            String sql6 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(6,'Hitsune','fire','MonsterJ1/HitsuneJ1.png','RivalMonster/Hitsune.png',130,50,20,'grass','steel','earth','water')";
-            String sql7 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(7,'Kazanne','fire','MonsterJ1/KazanneJ1.png','RivalMonster/Kazanne.png',165,30,35,'grass','steel','earth','water')";
-            String sql8 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(8,'Lifeuer','fire','MonsterJ1/LifeuerJ1.png','RivalMonster/Lifeuer.png',125,30,25,'grass','steel','earth','water')";
-            String sql9 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(9,'Nerite','water','MonsterJ1/NeriteJ1.png','RivalMonster/Nerite.png',190,48,28,'steel','fire','grass','earth')";
-            String sql10 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(10,'Phanseed','grass','MonsterJ1/Phanseed.png','RivalMonster/Phanseed.png',117,27,38,'water','earth','fire','steel')";
-            String sql11 = "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(11,'Pinp','water','MonsterJ1/PinpJ1.png','RivalMonster/Pinp.png',127,35,20,'steel','fire','grass','earth')";
-            String sql12= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(12,'Poolfie','water','MonsterJ1/PoolfieJ1.png','RivalMonster/Poolfie.png',165,33,36,'steel','fire','grass','earth')";
-            String sql13= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(13,'Sandbone','earth','MonsterJ1/SandboneJ1.png','RivalMonster/Sandbone.png',188,30,31,'fire','water','steel','grass')";
-            String sql14= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(14,'Sandeon','earth','MonsterJ1/Sandeon.png','RivalMonster/Sandeon.png',140,40,25,'fire','water','steel','grass')";
-            String sql15= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(15,'Searaw','water','MonsterJ1/SearawJ1.png','RivalMonster/Searaw.png',188,40,26,'steel','fire','grass','earth')";
-            String sql16= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(16,'Sheruha','grass','MonsterJ1/SheruhaJ1.png','RivalMonster/Sheruha.png',115,28,45,'water','earth','fire','steel')";
-            String sql17= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(17,'Spirunne','steel','MonsterJ1/SpirunneJ1.png','RivalMonster/Spirunne.png',185,41,29,'grass','earth','water','fire')";
-            String sql18= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(18,'Stail','steel','MonsterJ1/StailJ1.png','RivalMonster/Stail.png',200,20,50,'grass','earth','water','fire')";
-            String sql19= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(19,'Steeleon','steel','MonsterJ1/SteeleonJ1.png','RivalMonster/Steeleon.png',179,26,40,'grass','earth','water','fire')";
-            String sql20= "insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(20,'Tanuki','earth','MonsterJ1/TanukiJ1.png','RivalMonster/Tanuki.png',155,30,35,'fire','water','steel','grass')";
-            stmt.executeUpdate(sql1);
-            stmt.executeUpdate(sql2);
-            stmt.executeUpdate(sql3);
-            stmt.executeUpdate(sql4);
-            stmt.executeUpdate(sql5);
-            stmt.executeUpdate(sql6);
-            stmt.executeUpdate(sql7);
-            stmt.executeUpdate(sql8);
-            stmt.executeUpdate(sql9);
-            stmt.executeUpdate(sql10);
-            stmt.executeUpdate(sql11);
-            stmt.executeUpdate(sql12);
-            stmt.executeUpdate(sql13);
-            stmt.executeUpdate(sql14);
-            stmt.executeUpdate(sql15);
-            stmt.executeUpdate(sql16);
-            stmt.executeUpdate(sql17);
-            stmt.executeUpdate(sql18);
-            stmt.executeUpdate(sql19);
-            stmt.executeUpdate(sql20);
+            stmt = conn.createStatement();
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(1,'Amepach','steel','MonsterJ1/AmepachJ1.png','RivalMonster/Amepach.png',150,30,30,'grass','earth','water','fire')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(2,'Ankylore','earth','MonsterJ1/AnkyloreJ1.png','RivalMonster/Ankylore.png',190,25,45,'fire','water','steel','grass')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(3,'Haisand','earth','MonsterJ1/HaisandJ1.png','RivalMonster/Haisand.png',150,40,25,'fire','water','steel','grass')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(4,'Hakonmyo','steel','MonsterJ1/HakonmyoJ1.png','RivalMonster/Hakonmyo.png',160,45,25,'grass','earth','water','fire')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(5,'Hiprunio','fire','MonsterJ1/HirprunioJ1.png','RivalMonster/Hiprunio.png',170,30,25,'grass','steel','earth','water')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(6,'Hitsune','fire','MonsterJ1/HitsuneJ1.png','RivalMonster/Hitsune.png',130,50,20,'grass','steel','earth','water')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(7,'Kazanne','fire','MonsterJ1/KazanneJ1.png','RivalMonster/Kazanne.png',165,30,35,'grass','steel','earth','water')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(8,'Lifeuer','fire','MonsterJ1/LifeuerJ1.png','RivalMonster/Lifeuer.png',125,30,25,'grass','steel','earth','water')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(9,'Nerite','water','MonsterJ1/NeriteJ1.png','RivalMonster/Nerite.png',190,48,28,'steel','fire','grass','earth')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(10,'Phanseed','grass','MonsterJ1/Phanseed.png','RivalMonster/Phanseed.png',117,27,38,'water','earth','fire','steel')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(11,'Pinp','water','MonsterJ1/PinpJ1.png','RivalMonster/Pinp.png',127,35,20,'steel','fire','grass','earth')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(12,'Poolfie','water','MonsterJ1/PoolfieJ1.png','RivalMonster/Poolfie.png',165,33,36,'steel','fire','grass','earth')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(13,'Sandbone','earth','MonsterJ1/SandboneJ1.png','RivalMonster/Sandbone.png',188,30,31,'fire','water','steel','grass')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(14,'Sandeon','earth','MonsterJ1/Sandeon.png','RivalMonster/Sandeon.png',140,40,25,'fire','water','steel','grass')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(15,'Searaw','water','MonsterJ1/SearawJ1.png','RivalMonster/Searaw.png',188,40,26,'steel','fire','grass','earth')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(16,'Sheruha','grass','MonsterJ1/SheruhaJ1.png','RivalMonster/Sheruha.png',115,28,45,'water','earth','fire','steel')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(17,'Spirunne','steel','MonsterJ1/SpirunneJ1.png','RivalMonster/Spirunne.png',185,41,29,'grass','earth','water','fire')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(18,'Stail','steel','MonsterJ1/StailJ1.png','RivalMonster/Stail.png',200,20,50,'grass','earth','water','fire')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(19,'Steeleon','steel','MonsterJ1/SteeleonJ1.png','RivalMonster/Steeleon.png',179,26,40,'grass','earth','water','fire')");
+            stmt.executeUpdate("insert into bmonstruos(nummon,nmon,tipo,fotomon,fotomonr,hpmon,atkmon,defmon,fort1,fort2,deb1,deb2) values(20,'Tanuki','earth','MonsterJ1/TanukiJ1.png','RivalMonster/Tanuki.png',155,30,35,'fire','water','steel','grass')");
             conn.commit();
         } catch (SQLException ex) {
             System.err.println("No se pudieron insertar");
@@ -222,5 +195,49 @@ public class MetodosCrearBD {
         } catch (SQLException ex) {
             System.err.println("Error al mostrar la tabla bmonstruos");
         }
+    }
+    public static void insertarBMovimientos(){
+        try {
+            stmt=conn.createStatement();
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(1,'Absorber','planta',20,10)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(2,'Latigo cepa','planta',25,8)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(3,'Ciclon hojas','planta',30,7)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(4,'Hoja afilida','planta',35,5)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(5,'Gigadrenado','planta',40,4)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(6,'Rayo solar','planta',45,2)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(7,'Planta feroz','planta',50,1)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(8,'Burbujas','agua',20,10)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(9,'Pistola agua','agua',25,8)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(10,'Buceo','agua',30,7)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(11,'Escladar','agua',35,5)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(12,'Hidropulso','agua',40,4)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(13,'Hidrobomba','agua',45,2)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(14,'Hidrocañon','agua',50,1)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(15,'Ascuas','fuego',20,10)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(16,'Pirotecnia','fuego',25,8)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(17,'Rueda fuego','fuego',30,7)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(18,'Lanzallamas','fuego',35,5)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(19,'Llamarada','fuego',40,4)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(20,'Infierno','fuego',45,2)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(21,'Anillo igneo','fuego',50,1)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(22,'Cuerpo pesado','acero',20,10)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(23,'Foco resplandor','acero',25,8)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(24,'Cola ferrea','acero',30,7)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(25,'Garra metal','acero',35,5)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(26,'Cabeza hierro','acero',40,4)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(27,'Represion metal','acero',45,2)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(28,'Bomba iman','acero',50,1)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(29,'Bucle arena','tierra',20,10)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(30,'Bofeton lodo','tierra',25,8)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(35,'Excavar','tierra',30,7)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(36,'Bomba fango','tierra',35,5)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(37,'Tierra viva','tierra',40,4)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(38,'Terremoto','tierra',45,2)");
+            stmt.executeUpdate("insert into bmovimientos(nummov,nmov,tipo,daño,usos)values(39,'Fisura','tierra',50,1)");
+            conn.commit();
+        } catch (SQLException ex) {
+            Logger.getLogger(MetodosCrearBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
