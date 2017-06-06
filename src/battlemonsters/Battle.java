3bcd5d7f61a0089e2f3728c2e379ed873b19b5f1
AlonsoPerezDiego <Dialpe22@gmail.com>
2017-06-06 18:39:41 +0200

    Constructor terminado.

--- src/battlemonsters/Battle.java
+++ src/battlemonsters/Battle.java
@@ -12,12 +12,33 @@ package battlemonsters;
 public class Battle {
     private String[] myStr, rStr, myWeak, rWeak;
     private int myHP, myAttack, myDefense, myLostHP, rHP, rAttack, rDefense, rLostHP;
-    private String myPic, rPic, myNMon, rNMon;
+    private String myPic, rPic, myNMon, rNMon, myType, rType;
     private float myEffic, rEffic;
     private boolean victory, defeat;
     
     public Battle(int myMonster, int rMonster){
-        
+        myNMon = MetodosCrearBD.selectMonster(myMonster, 0);
+        myType = MetodosCrearBD.selectMonster(myMonster, 1);
+        myPic = MetodosCrearBD.selectMonster(myMonster, 2);
+        myHP = Integer.parseInt(MetodosCrearBD.selectMonster(myMonster, 4));
+        myAttack = Integer.parseInt(MetodosCrearBD.selectMonster(myMonster, 5));
+        myDefense = Integer.parseInt(MetodosCrearBD.selectMonster(myMonster, 6));
+        myLostHP = myHP;
+        myStr[0] = MetodosCrearBD.selectMonster(myMonster, 7);
+        myStr[1] = MetodosCrearBD.selectMonster(myMonster, 8);
+        myWeak[0] = MetodosCrearBD.selectMonster(myMonster, 9);
+        myWeak[1] = MetodosCrearBD.selectMonster(myMonster, 10);
+        rNMon = MetodosCrearBD.selectMonster(rMonster, 0);
+        rType = MetodosCrearBD.selectMonster(rMonster, 1);
+        rPic = MetodosCrearBD.selectMonster(rMonster, 3);
+        rHP = Integer.parseInt(MetodosCrearBD.selectMonster(rMonster, 4));
+        rAttack = Integer.parseInt(MetodosCrearBD.selectMonster(rMonster, 5));
+        rDefense = Integer.parseInt(MetodosCrearBD.selectMonster(rMonster, 6));
+        rLostHP = rHP;
+        rStr[0] = MetodosCrearBD.selectMonster(rMonster, 7);
+        rStr[1] = MetodosCrearBD.selectMonster(rMonster, 8);
+        rWeak[0] = MetodosCrearBD.selectMonster(rMonster, 9);
+        rWeak[1] = MetodosCrearBD.selectMonster(rMonster, 10);
     }
     
     
