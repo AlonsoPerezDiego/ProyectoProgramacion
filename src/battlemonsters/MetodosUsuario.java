561ef12ced5b104b424013ef95ef01ba17b33c3f
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-06-08 14:04:32 +0200

    Metodo para mostrar usuario y borrar en una nueva partida

--- src/battlemonsters/MetodosUsuario.java
+++ src/battlemonsters/MetodosUsuario.java
@@ -5,10 +5,44 @@
  */
 package battlemonsters;
 
+import static battlemonsters.MetodosCrearBD.*;
+import interfaces.NewGame;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 /**
  *
  * @author Adry
  */
 public class MetodosUsuario {
-    
+    public static void mostrarDatosJugador(){
+        try {
+            stmt=conn.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from jugador where numxogador="+NewGame.codPartidaLabel.getText()+";");
+            NewGame.nombreLabel.setText(rs.getString("njugador"));
+            NewGame.victoriasLabel.setText("Nº de victorias: "+rs.getInt("pwin"));
+            NewGame.derrotasLabel.setText("Nª de derrota: "+rs.getInt("plose"));
+            if(rs.getString("sjugador")=="h"){
+                NewGame.fotoLabel.setIcon(new ImageIcon("src/images/Boy.jpg"));
+            }else if(rs.getString("sjugador")=="m"){
+                NewGame.fotoLabel.setIcon(new ImageIcon("src/images/Girl.jpg"));
+            }
+        } catch (SQLException ex) {
+            System.err.println("Fallo al cargar jugador");
+        }
+    }
+    public static void borrarJugador(){
+        try {
+            stmt=conn.createStatement();
+            stmt.executeUpdate("delete from jugador where numxogador="+NewGame.codPartidaLabel.getText()+";");
+        } catch (SQLException ex) {
+            try {
+                stmt.executeUpdate("insert into jugador(numxogador,njugador,sjugador,pwin,plose)values("+NewGame.codPartidaLabel.getText()+",'Partida vacia','desconocido',0,0);");
+            } catch (SQLException ex1) {
+                System.err.println("Error al insertar jugador vacio");;
+            }
+        }
+    }
 }
